c96517bb7618369b85bd4b78b7399d6ce36828ca
Mike O'Brien <mob@mikeobrien.net>
2019-12-30 16:52:45 -0600

    Set up CI/CD with Azure Pipelines

--- node_modules/grunt-dotnet-assembly-info/tasks/regex.js
+++
@@ -1,9 +0,0 @@
-exports.matchAll = function(regex, text) {
-    var matches = [];
-    var match = regex.exec(text);
-    while (match !== null) {
-        matches.push(match[1]);
-        match = regex.exec(text);
-    }
-    return matches;
-};
